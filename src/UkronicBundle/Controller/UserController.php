1c0f5aedd528054d19df6abd1246e96be1fe8cc0
François <francois.lavazec@orange.fr>
2017-03-24 13:06:49 +0100

    Mise en conformité composer.json

--- src/UkronicBundle/Controller/UserController.php
+++ src/UkronicBundle/Controller/UserController.php
@@ -9,10 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use UkronicBundle\Entity\Recherche;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 
